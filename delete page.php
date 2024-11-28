d94f3608ca5c2720c3878fd1d9b7d98febc6ec5c
Enrique-UP <95688780+Enrique-UP@users.noreply.github.com>
2024-11-28 22:17:34 +0530

    Delete delete page.php
    code

--- delete page.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<!-- <link rel="icon" type="image/png" href="images/favicon.png" />
-<link rel="icon" type="image/x-icon" href="images/favicon.ico" /> -->
-<link rel="stylesheet" href="<?=$siteUrl?>/css/style.css?0" type="text/css" />
